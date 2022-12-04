ed55818010e8afb563a13c6e644636d62c684384
rafal-ftw <rafal.w.draws@gmail.com>
2022-12-04 16:33:41 +0100

    orm scenario to db

--- wats/views.py
+++ wats/views.py
@@ -1,21 +1,24 @@
+from datetime import datetime
 from flask import render_template, request, Response
 
 from models import Scenario, PossibleStep, create_scenario
 from app import app
-import pprint
+
 
 @app.route("/")
 def index():
     return render_template('index.html')
 
+
 @app.route("/scenario_list", methods=["GET"])
 def scenario_list():
-    
+
     scenarios = Scenario.query.all()
 
-    return render_template('scenario_list.html', scenarios = scenarios)
+    return render_template('scenario_list.html', scenarios=scenarios)
 
-@app.route("/scenario_add", methods = ["GET", "POST"])
+
+@app.route("/scenario_add", methods=["GET", "POST"])
 def scenario_add():
 
     steps = PossibleStep.query.all()
@@ -23,39 +26,47 @@ def scenario_add():
     step_names = []
     for i in steps:
         step_names.append(i.name)
-    
 
     if request.method == "GET":
-        return render_template('scenario_add.html', steps = steps)
+        return render_template('scenario_add.html', steps=steps)
 
-     
     scenario_name = request.form.get('name_field')
     scenario_expected = request.form.get('expected_field')
     scenario_author = request.form.get('author')
-    
+
     scenario_details = [scenario_name, scenario_expected, scenario_author]
 
-    return render_template('add_steps.html', scenario_details = scenario_details, step_names = step_names)
+    return render_template('add_steps.html', scenario_details=scenario_details, step_names=step_names)
 
-@app.route("/scenario_added", methods = ["GET", "POST"])
+
+@app.route("/scenario_added", methods=["GET", "POST"])
 def scenario_added():
 
     if request.method == "GET":
-        return '<h6>bad request, it was get for /scenario_added</h6>' #TODO 404
+        return '<h6>bad request, it was get for /scenario_added</h6>'  # TODO 404
 
     print(request.form.keys())
     print(request.form.items())
     scenario_data = request.form
-    return render_template('scenario_added.html', data = scenario_data)
+    return render_template('scenario_added.html', data=scenario_data)
 
-@app.route("/scenario_add_test", methods = ["GET", "POST"])
+
+@app.route("/scenario_add_test", methods=["GET", "POST"])
 def scenario_add_test():
 
     data = request.get_json()
-    
+
     print(data)
     for key in data:
-        print(data[key])
-    
-    
+        print(f'{datetime.now()} - {key}', f'| data : {data[key]}\n\n')
+
+    created_scenario = create_scenario(
+        data['scenario_name'],
+        data['expected'],
+        str(data['steps']),
+        data['author_name'])
+
+    print('scenario has been added!')
+    print(created_scenario)
+
     return 'redirected at test'
